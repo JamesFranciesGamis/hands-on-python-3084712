47b88c06c1d96cef48c430f6955a5779d016bd12
James Francies Gamis <110861311+JamesFranciesGamis@users.noreply.github.com>
2022-11-29 15:27:17 +0000

    added sentences

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,15 +4,15 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+JOHN_PAUL = ''.join(NAMES[:2])
+GEORGE_RINGO = ''.join(NAMES[2:])
+REVERSE = ''.join(NAMES[::-1])
+EVERY_OTHER = ''.join(NAMES[::2])
 
-print(sum(AGES))
-print(min(AGES))
-print(max(AGES))
+print("Here is the sum of the ages " + str(sum(AGES)))
+print("Here is the youngest value of ages " + str(min(AGES)))
+print("Here is the maximum value of ages " + str(max(AGES)))
 
-print(JOHN_PAUL)
-print(GEORGE_RINGO)
-print(REVERSE)
+print("His name is " + JOHN_PAUL)
+print("The other guy is named " + GEORGE_RINGO)
+print("Here is the reverse of names " + REVERSE)
